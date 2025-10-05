9a42dde563983dc10ea11a3267dde65b55834690
mihir021 <rathodmihir1113@gmail.com>
2025-10-05 17:59:36 +0530

    leet code

--- Leet11.java
+++ Leet11.java
@@ -1,18 +1,16 @@
 class Solution11 {
-    public int maxArea(int[] height) {
+    public static int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
         int maxArea = 0;
 
         while (left < right) {
 
-            // calculations
             int minHeight = Math.min(height[left], height[right]);
             int width = right - left;
             int currentArea = minHeight * width;
             maxArea = Math.max(maxArea, currentArea);
 
-            // incriment oprations
             if (height[left] < height[right]) {
                 left++;
             } else {
@@ -21,5 +19,10 @@ class Solution11 {
         }
 
         return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int ans = maxArea(new int[]{3,1,10,6,11,2,3,4,5,1,1,1,1,1,1,1,1,3});
+        System.out.println(ans);
     }
 }
