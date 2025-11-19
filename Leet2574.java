577ec8c0d3ab58f023a0140aed616c9cb2ff4f0c
mihir021 <rathodmihir1113@gmail.com>
2025-11-19 20:30:23 +0530

    leet code

--- Leet2574.java
+++ Leet2574.java
@@ -1,15 +1,15 @@
 class Solution2574 {
     public int[] leftRightDifference(int[] nums) {
-        int[] leftsum = new int[nums.length];
+        int[] leftSum = new int[nums.length];
         int sum = 0;
         int temp = 0;
         for(int i = 0; i< nums.length; i++){
             sum += nums[i];
-            leftsum[i] = temp;
+            leftSum[i] = temp;
             temp += nums[i];
         }
         for(int i = 0; i< nums.length; i++){
-            nums[i] = Math.abs(leftsum[i] - (sum - leftsum[i]-nums[i]));
+            nums[i] = Math.abs(leftSum[i] - (sum - leftSum[i]-nums[i]));
         }
         return nums;
     }
