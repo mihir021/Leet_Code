857a744de9a006ba1d933ab33cb38a1ce2db2753
mihir021 <rathodmihir1113@gmail.com>
2025-11-25 20:24:10 +0530

    leet code

--- Leet1015.java
+++ Leet1015.java
@@ -1,15 +1,15 @@
 public class Leet1015 {
     public int smallestRepunitDivByK(int k) {
-       if(k % 2 == 0 || k % 5 == 0) return -1;
+        if(k % 2 == 0 || k % 5 == 0) return -1;
         int rem = 1;
-        int c = 0;
-       while(true){
+        int c = 1;
+        if(1%k==0) return c;
+        while(true){
             rem = (rem*10+1)%k;
+            c++;
             if(rem == 0){
                 return c;
             }
-            c++;
-       }
-
+        }
     }
 }
