7e7b7dc051545f5a311074d998119821cdfc33ad
mihir021 <rathodmihir1113@gmail.com>
2025-11-27 19:09:52 +0530

    leet code

---
+++ Leet3381.java
@@ -0,0 +1,18 @@
+public class Leet3381 {
+    static void main(String[] args) {
+
+    }
+    int maxSum(int[] nums , int w){
+        int max = Integer.MIN_VALUE;
+        int current = 0;
+        int sum = 0;
+        while (current < w){
+            sum += nums[current];
+        }
+        max = sum;
+        for (int i = 0;;){
+
+        }
+
+    }
+}
