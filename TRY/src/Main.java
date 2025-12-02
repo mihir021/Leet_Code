24fb35b4af28f96ff7570da0e99f370e2b78644f
Mihir Rathod <rathodmihir1113@gmail.com>
2025-12-02 12:45:32 +0530

    Auto push at 02-12-2025 12:45:32.19

--- TRY/src/Main.java
+++ TRY/src/Main.java
@@ -5,14 +5,7 @@ import java.util.Set;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> s = new HashSet<>();
-        s.add(1);
-        s.add(2);
-        s.add(1);
-        s.add(3);
-        s.add(1);
-        for(int i = 0 ; i < s.size() ; i++){
-            System.out.println();
-        }
+
+        throw ArithmeticException;
     }
 }
