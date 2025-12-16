190464edcf23cfe6c46322a35a7720b0dd04386a
mihir021 <rathodmihir1113@gmail.com>
2025-12-16 22:48:03 +0530

    leet code

---
+++ src/Leet1221.java
@@ -0,0 +1,12 @@
+class Solution1221 {
+    public int balancedStringSplit(String s) {
+        int count = 0;
+        int ans = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)=='R') count++;
+            else count--;
+            if(count==0) ans++;
+        }
+        return ans;
+    }
+}
