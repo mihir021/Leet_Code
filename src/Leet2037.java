33dd9fd7bc11dbb370572566b9607d1b8ebb6d99
mihir021 <rathodmihir1113@gmail.com>
2025-12-19 19:20:32 +0530

    leet code

---
+++ src/Leet2037.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution2037 {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int ans = 0;
+        for (int i = 0 ; i < seats.length ; i++){
+            ans += (Math.abs(students[i]-seats[i]));
+        }
+        return ans;
+    }
+}
