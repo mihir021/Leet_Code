14d6ffe316f4c6bca9cbb78d9bba12332cfd1537
mihir021 <rathodmihir1113@gmail.com>
2025-12-27 18:45:32 +0530

    Auto push at 27-12-2025 18:45:32.38

--- src/Leet227.java
+++ src/Leet227.java
@@ -1,27 +1,48 @@
 import java.util.Stack;
 
 class Solution227 {
-    public int calculate(String s) {
+    public static int calculate(String s) {
         Stack<Integer> ans = new Stack<>();
         s = s.trim();
+        if (s.length() == 1){
+            return s.charAt(0)-'0';
+        }
         char currentElement;
         char nextElement;
+        String temp="";
+        boolean fix;
         for (int i = 0 ; i < s.length()-1 ; i++){
             currentElement = s.charAt(i);
+            if (currentElement==' ') continue;
+            while (i < s.length()-1 && s.charAt(i+1)== ' '){
+               i++;
+            }
             nextElement = s.charAt(i+1);
-
+            if (Character.isDigit(currentElement) && Character.isDigit(nextElement)){
+                temp = currentElement +""+ nextElement;
+            }
+// 1 2 9 8 9 8 9  + 3
+// c n
             if (Character.isDigit(currentElement)){
                 ans.push(currentElement-'0');
             } else if (currentElement == '*') {
                 ans.push(ans.pop()*(nextElement-'0'));
             } else if (currentElement == '/') {
                 ans.push(ans.pop()/(nextElement-'0'));
-            } else if (currentElement == '+') {
-                ans.push((-'0'));
+            } else if (currentElement == '-') {
+               ans.push(-(nextElement-'0'));
+               i++;
             }
         }
-        // + -
-        // 1 + 2 * 3  +4
-        // 1  6
+        // 1 - 2 + 3 * 4 / 6
+        int finalAns = 0;
+        while (!ans.isEmpty()){
+            finalAns += ans.pop();
+        }
+        return finalAns;
+    }
+
+    static void main(String[] args) {
+        System.out.println(calculate(" 3+5 / 2 "));
     }
 }
