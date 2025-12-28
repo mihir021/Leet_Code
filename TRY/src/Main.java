4f695421cc09cc1763d0dd65991a4fd403e160d8
mihir021 <rathodmihir1113@gmail.com>
2025-12-28 21:45:32 +0530

    Auto push at 28-12-2025 21:45:31.92

--- TRY/src/Main.java
+++ TRY/src/Main.java
@@ -1,4 +1,6 @@
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,6 +8,34 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
-        throw ArithmeticException;
+    }
+    int minTime(int n , int[] rank){
+        PriorityQueue<My> pq = new PriorityQueue<>(Comparator.comparing(My::getRank));
+        int ans = 0;
+        for (int i : rank){
+            pq.add(new My(1,i));
+        }
+        while (n > 0){
+            My minTime = pq.remove();
+            ans += minTime.rank;
+            minTime.count++;
+            minTime.rank = minTime.rank*minTime.count;
+            pq.add(minTime);
+            n--;
+        }
+        return ans;
+    }
+    class My{
+        int count;
+        int rank;
+
+        public My(int count, int rank) {
+            this.count = count;
+            this.rank = rank;
+        }
+
+        public int getRank() {
+            return rank;
+        }
     }
 }
