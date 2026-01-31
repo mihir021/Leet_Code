c4ed1fda8c4f97e1ea259e33f46554bd4a90b5d2
mihir021 <rathodmihir1113@gmail.com>
2026-01-31 22:33:03 +0530

    leet code

---
+++ src/Leet744.java
@@ -0,0 +1,12 @@
+class Solution744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char ans = letters[0];
+        for(int i = 0; i<letters.length; i++){
+            if(letters[i]>target){
+                ans = letters[i];
+                break;
+            }
+        }
+        return ans;
+    }
+}
